713e049f9e31f8ebef3795b573c450968cbddff2
zatty-5118 <ryosuke.29.m@outlook.jp>
2024-05-12 15:57:11 +0900

    first commit

---
+++ src/scripts/modules/array__allBrand.js
@@ -0,0 +1,22 @@
+export const array__allBrand = [
+    {
+        brand: "HG",
+        brandSlug:"hg",
+    },
+    {
+        brand: "RG",
+        brandSlug:"rg",
+    },
+    {
+        brand: "MG",
+        brandSlug:"mg",
+    },
+    {
+        brand: "RE/100",
+        brandSlug:"re100",
+    },
+    {
+        brand: "FULL MECHANICS",
+        brandSlug:"fullmechanics",
+    },
+]
---
+++ src/scripts/modules/array__allSeries.js
@@ -0,0 +1,110 @@
+export const array__allSeries = [
+    {
+        series: "ガンダムベース",
+        seriesJa: "ガンダムベース限定",
+        seriesSlug:"gundam_base",
+        century: ""
+    },
+    {
+        series: "水星の魔女",
+        seriesJa: "機動戦士ガンダム 水星の魔女",
+        seriesSlug:"twfm",
+        century: "another"
+    },
+    {
+        series: "ビルドダイバーズ",
+        seriesJa: "機動戦士ガンダム ビルドダイバーズ",
+        seriesSlug:"build_divers",
+        century: "another"
+    },
+    {
+        series: "00",
+        seriesJa: "機動戦士ガンダム00 シリーズ",
+        seriesSlug:"00",
+        century: "another"
+    },
+    {
+        series: "SEED FREEDOM",
+        seriesJa: "機動戦士ガンダムSEED FREEDOM",
+        seriesSlug:"seed_freedom",
+        century: "another"
+    },
+    {
+        series: "SEED DESTINY",
+        seriesJa: "機動戦士ガンダムSEED DESTINY",
+        seriesSlug:"seed_destiny",
+        century: "another"
+    },
+    {
+        series: "SEED",
+        seriesJa: "機動戦士ガンダムSEED",
+        seriesSlug:"seed",
+        century: "another"
+    },
+    {
+        series: "Endless Waltz",
+        seriesJa: "新機動戦記ガンダムW Endless Waltz",
+        seriesSlug:"ew",
+        century: "another"
+    },
+    {
+        series: "W",
+        seriesJa: "新機動戦記ガンダムW",
+        seriesSlug:"w",
+        century: "another"
+    },
+    {
+        series: "ミッシングリンク",
+        seriesJa: "機動戦士ガンダム外伝 ミッシングリンク",
+        seriesSlug:"missinglink",
+        century: "uc"
+    },
+    {
+        series: "サンダーボルト",
+        seriesJa: "機動戦士ガンダム サンダーボルト",
+        seriesSlug:"thunderbolt",
+        century: "uc"
+    },
+    {
+        series: "ポケットの中の戦争",
+        seriesJa: "機動戦士ガンダム ポケットの中の戦争",
+        seriesSlug:"pocket",
+        century: "uc"
+    },
+    {
+        series: "センチネル",
+        seriesJa: "機動戦士ガンダム・センチネル",
+        seriesSlug:"sentinel",
+        century: "uc"
+    },
+    {
+        series: "ユニコーン",
+        seriesJa: "機動戦士ガンダム ユニコーン",
+        seriesSlug:"unicorn",
+        century: "uc"
+    },
+    {
+        series: "逆襲のシャア",
+        seriesJa: "機動戦士ガンダム 逆襲のシャア",
+        seriesSlug:"cca",
+        century: "uc"
+    },
+    {
+        series: "ZZ",
+        seriesJa: "機動戦士ZZガンダム",
+        seriesSlug:"zz",
+        century: "uc"
+    },
+    {
+        series: "Z",
+        seriesJa: "機動戦士Zガンダム",
+        seriesSlug:"z",
+        century: "uc"
+    },
+    {
+        series: "1st",
+        seriesJa: "機動戦士ガンダム",
+        seriesSlug:"1st",
+        century: "uc"
+    },
+]
